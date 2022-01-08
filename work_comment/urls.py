808755e005b231345d73afe373d33e3a119c7974
Balsi <chooth333@gmail.com>
2022-01-09 00:17:39 +0800

    '1'

--- work_comment/urls.py
+++ work_comment/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("all_comments/", views.all_comments),
     path("write/", views.write_comment),
+    path("single_comments/",views.singel_comment)
     # path("comment/<int:id>"),
 ]
